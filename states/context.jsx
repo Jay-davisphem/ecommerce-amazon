1eddb36beb32e5b6220099abd5c3489fac3f9b8b
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-07-09 01:23:42 +0100

    fix typos

--- states/context.jsx
+++ states/context.jsx
@@ -2,7 +2,7 @@ import { createContext, useReducer } from "react";
 import actions from "./actions";
 import reducer from "./reducer";
 
-export const AppContext = createContexti();
+export const AppContext = createContext();
 
 const initialState = {
   data: [],
